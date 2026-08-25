8ee0ee8ba75a09bf354ed758629c16f4b666fe4c
WackyWackSam <samuel.sheriff987@gmail.com>
2026-08-25 19:33:42 +0000

    Aug 25, 2026, 3:33 PM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
 
-          <main className="flex-1">
+          <main className="flex-1 pt-20">
             {children}
           </main>
 
